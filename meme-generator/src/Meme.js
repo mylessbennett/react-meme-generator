03bcc3c56262c1ed4a34cb07fdd171f6471ba775
Myles Bennett <mylessbennett@gmail.com>
2019-09-04 23:19:24 -0400

    added buttons to change caption color/more styles

--- meme-generator/src/Meme.js
+++ meme-generator/src/Meme.js
@@ -30,13 +30,34 @@ function Meme () {
     const captionElements = captionArray.map((captionObj) => <Textbox className="caption-input" key={captionArray.indexOf(captionObj)} captionArray={captionArray} />)
 
     function handleAddCaption() {
-        setCaptionArray(() => [...captionArray, {placeholder: 'test'}]);
-        console.log(captionArray)
+        if (captionArray.length < 3) {
+            setCaptionArray(() => [...captionArray, {placeholder: 'test'}]);
+        }
     }
 
     function handleDeleteCaption() {
         const tempArray = captionArray
         setCaptionArray(tempArray.slice(0, -1))
+    }
+
+    function handleChangeColourBlack() {
+        const text = document.getElementsByClassName("caption-text")
+        console.log(text)
+        for (let i = 0;i < text.length; i++) {
+            if (!(text[i].classList.contains('caption-text-black'))) {
+                text[i].classList.add('caption-text-black')
+            }
+        }
+    }
+
+    function handleChangeColourWhite() {
+        const text = document.getElementsByClassName("caption-text")
+        console.log(text)
+        for (let i = 0;i < text.length; i++) {
+            if ((text[i].classList.contains('caption-text-black'))) {
+                text[i].classList.remove('caption-text-black')
+            }
+        }
     }
 
     return (
@@ -48,6 +69,8 @@ function Meme () {
                 <div>
                     <ul className="caption-list">
                         <li>{ captionElements }</li>
+                        <li><button className="change-clr-btn-blk" onClick={handleChangeColourBlack} title="Change Caption Colours Black"></button></li>
+                        <li><button className="change-clr-btn-wht" onClick={handleChangeColourWhite} title="Change Caption Colours White"></button></li>
                         <li><button className="add-btn" onClick={handleAddCaption}>Add Caption</button></li>
                         <li><button className="del-btn" onClick={handleDeleteCaption}>Delete Caption</button></li>
                     </ul>
